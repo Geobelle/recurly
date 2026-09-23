f997f10c56a6b9587c99cbd3901ae23ac2c7a51d
Georgina Riebelle <georginariebelle@gmail.com>
2026-09-23 19:59:26 +0100

    ui for home

---
+++ components/UpcomingSubscriptionCards.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const UpcomingSubscriptionCards = () => {
+  return (
+    <View>
+      <Text>UpcomingSubscriptionCards</Text>
+    </View>
+  );
+};
+
+export default UpcomingSubscriptionCards;

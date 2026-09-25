a27f70dc346ccb1d1ae06192c363931bca870996
Georgina Riebelle <georginariebelle@gmail.com>
2026-09-25 01:39:33 +0100

    more home ui

--- components/UpcomingSubscriptionCards.tsx
+++ components/UpcomingSubscriptionCards.tsx
@@ -1,9 +1,30 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { formatCurrency } from "../lib/utils";
 
-const UpcomingSubscriptionCards = () => {
+const UpcomingSubscriptionCards = ({
+  name,
+  price,
+  daysLeft,
+  currency,
+  icon,
+}: UpcomingSubscription) => {
   return (
-    <View>
-      <Text>UpcomingSubscriptionCards</Text>
+    <View className="upcoming-card">
+      <View className="upcoming-row">
+        <Image source={icon} className="upcoming-icon" />
+        <View>
+          <Text className="upcoming-price">
+            {formatCurrency(price, currency)}
+          </Text>
+          <Text className="upcoming-meta" numberOfLines={1}>
+            {" "}
+            {daysLeft > 1 ? `${daysLeft} days left` : "Last day"}{" "}
+          </Text>
+        </View>
+      </View>
+      <Text className="upcoming-name" numberOfLines={1}>
+        {name}
+      </Text>
     </View>
   );
 };
